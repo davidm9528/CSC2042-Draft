8a3c536ba9ccb00327a6ba3db09a966960586a2a
David Mackenzie <davidm9528@gmail.com>
2019-10-10 14:25:42 +0100

    Add files via upload
    Leases table partially added, started fixing and changes FK's

--- tables.sql
+++ tables.sql
@@ -16,14 +16,25 @@ FOREIGN KEY ('Paid_Status') References 'Account'('Paid_Status')
 
 
 
+--Building--
+CREATE TABLE IF NOT EXISTS 'Building' (
+'Building_ID' int NOT NULL AUTO_INCREMENT UNIQUE,
+'Building_Name' varchar(3),
+'Building_Address' char,
+'Building_PostCode' varchar(7) UNIQUE,
+PRIMARY KEY ('Building_ID')
+FOREIGN KEY ('Manager_ID') References 'Manager' ('Manager_ID')
+FOREIGN KEY ('Apartment_ID') References 'Apartment' ('Apartment_ID');
+);
+
+
 --Bank Account--
 CREATE TABLE IF NOT EXISTS 'Bank Account' (
-'Account_ID' int NOT NULL AUTO_INCREMENT UNIQUE,
+'BankAccount_ID' int NOT NULL AUTO_INCREMENT UNIQUE,
 FOREIGN KEY ('Tenant_FName') References 'Tenant'('Tenant_FName'),
 FOREIGN KEY ('Tenant_SName') References 'Tenant'('Tenant_SName'),
 'Account_Number' smallint NOT NULL, 
 'Sort_Code' smallint NOT NULL,
-'Paid_Status' BIT --Yes or no
 );
 
 
@@ -35,8 +46,20 @@ CREATE TABLE IF NOT EXISTS 'Apartment' (
 'Apartment_Type' varchar(32),
 'Apartment_HMO' BIT, --Yes or no
 PRIMARY KEY ('Appartment_Block'),
-FOREIGN KEY ('Tenant_ID') References 'Tenant' ('Tenant_ID'),
-FOREIGN KEY ('Building_ID') References 'Building' ('Building_ID'),;
+FOREIGN KEY ('Lease_ID') References 'Lease' ('Lease_ID'),
+FOREIGN KEY ('Building_ID') References 'Building' ('Building_ID'),
+);
+
+
+
+--Lease--
+CREATE TABLE IF NOT EXISTS 'Lease' (
+'Lease_ID' int NOT NULL AUTO_INCREMENT UNIQUE,
+'Lease_Status'  BIT, --Yes or no (Paid or not paid)
+'Lease_Cost' int NOT NULL,
+FOREIGN KEY ('Tenant_ID') References 'Tenant'('Tenant_ID'),
+FOREIGN KEY ('Apartment_ID') References 'Apartment' ('Apartment_ID'),
+FOREIGN KEY ('Admin_ID') References 'Admin' ('Admin_ID')
 );
 
 
@@ -61,11 +84,8 @@ CREATE TABLE IF NOT EXISTS 'Manager'
 'Manager_ID'int NOT NULL AUTO_INCREMENT UNIQUE,
 'Manager_Fname' char,
 'Manager_Sname' char,
------
------
 PRIMARY KEY ('Manager_ID'),
-FOREIGN KEY ('Building_ID') References 'Building' ('Building_ID'),
-FOREIGN KEY ('Apartment_Block') References 'Apartment' ('Apartment_Block')
+FOREIGN KEY ('Building_ID') References 'Building' ('Building_ID');
 );
 
 
@@ -73,9 +93,6 @@ FOREIGN KEY ('Apartment_Block') References 'Apartment' ('Apartment_Block')
 --Admin--
 CREATE TABLE IF NOT EXISTS 'Admin' (
 'Admin_ID'int NOT NULL AUTO_INCREMENT UNIQUE,
------
------
------
 PRIMARY KEY ('Admin_ID'),
 );
 
@@ -84,10 +101,8 @@ PRIMARY KEY ('Admin_ID'),
 --Jobs/Contracts--
 CREATE TABLE IF NOT EXISTS 'Jobs And Contracts'
 'JaC_ID'int NOT NULL AUTO_INCREMENT UNIQUE,
------
------
------
 PRIMARY KEY ('JaC_ID'),
+FOREIGN KEY 'Technicians' REFERENCES 'Technicians' ('Technicians_ID')
 );
 
 
@@ -95,8 +110,7 @@ PRIMARY KEY ('JaC_ID'),
 --Technicians--
 CREATE TABLE IF NOT EXISTS 'Technicians'
 'Technicians_ID'int NOT NULL AUTO_INCREMENT UNIQUE,
------
------
------
 PRIMARY KEY ('Technicians_ID'),
+FOREIGN KEY 'Employee_ID' References 'Employee' ('Employee_ID')
+FOREIGN KEY 'JaC_ID' References 'Jobs And Contracts' ('JaC_ID')
 );
