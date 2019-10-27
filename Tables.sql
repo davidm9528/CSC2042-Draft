dd18c36c780f04cd77a4ffa69c4deede943d2ea3
David Mackenzie <davidm9528@gmail.com>
2019-10-27 20:35:46 +0000

    Add files via upload
    Tables amended

--- Tables.sql
+++ Tables.sql
@@ -9,8 +9,7 @@ CREATE TABLE IF NOT EXISTS 'Person' (
 'DoB' date, 
 'ContactNo' varchar(11),
 'NextOfKinNo' varchar(11),
-'StuNumber' int varchar(8) UNIQUE, 
-'Diabilities' BIT,
+'Diabilities' BOOLEAN,
 PRIMARY KEY ('PersonID'),
 FOREIGN KEY ('TenantID') References 'Tenant' ('TenantID'),
 FOREIGN KEY ('EmployeeID') References 'Employee' ('EmployeeID'),
@@ -24,7 +23,6 @@ CREATE TABLE IF NOT EXISTS 'Employee' (
 'EmployeeID' int NOT NULL AUTO_INCREMENT UNIQUE,
 FOREIGN KEY ('PersonID') References 'Person' ('PersonID'),
 FOREIGN KEY ('PaymentsOutgoingREF') References 'PaymentsOutgoing' ('PaymentsOutgoingREF'),
---FOREIGN KEY ('BuildingID') References 'Building' ('BuildingID') --Connection not in ERD, discuss...
 FOREIGN KEY ('TechniciansID') References 'Technicians' ('TechniciansID'),
 FOREIGN KEY ('ManagerID') References 'Manager' ('ManagerID'),
 FOREIGN KEY ('EmployeeContractID') References 'EmployeeContract' ('EmployeeContractID'),
@@ -40,9 +38,9 @@ PRIMARY KEY ('TenantID'),
 FOREIGN KEY ('PersonID') References 'Person' ('PersonID'),
 FOREIGN KEY ('VisitorID') References 'Visitor' ('VisitorID'),
 FOREIGN KEY ('LeaseID') References 'Lease' ('LeaseID'),
-FOREIGN KEY ('PaymentsIncomingREF') References 'PaymentsIncoming'('PaymentsIncomingREF'),
---FK (Employee) ?????????
+FOREIGN KEY ('PaymentsIncomingREF') References 'PaymentsIncoming'('PaymentsIncomingREF')
 );
+
 
 
 --Bank Account--
@@ -56,15 +54,14 @@ FOREIGN KEY ('PaymentsIncomingREF') References 'PaymentsIncoming' ('PaymentsInco
 );
 
 
---Not on ERD-------------------------------------------------------------------
+
 --Visitor
 CREATE TABLE IF NOT EXISTS 'Visitor' (
-'VisitorID' int NOT NULL AUTO_INCREMENT UNIQUE,
+--FOREIGN KEY ('TenantID') References 'Tenant' ('TenantID'),
+FOREIGN KEY ('LeaseID') References 'Lease' ('LeaseID'),
 'VFName' char,
 'VSName' char,
-'Duration' TIME NOT NULL,
-FOREIGN KEY ('TenantID') References 'Tenant' ('TenantID'),
---FOREIGN KEY ('ApartmentID') References 'Apartment' ('ApartmentID')
+'Duration' TIME NOT NULL
 );
 
 
@@ -76,8 +73,6 @@ CREATE TABLE IF NOT EXISTS 'Building' (
 'Address' char,
 'PostCode' varchar(7) UNIQUE,
 PRIMARY KEY ('BuildingID'),
---BOTH???
---FOREIGN KEY ('ManagerID') References 'Manager' ('ManagerID'),
 FOREIGN KEY ('ApartmentID') References 'Apartment' ('ApartmentID')
 );
 
@@ -88,12 +83,12 @@ CREATE TABLE IF NOT EXISTS 'Apartment' (
 'ApartmentID' int NOT NULL AUTO_INCREMENT UNIQUE,
 'Block' varchar(32),
 'Type' varchar(32),
-'HMO' BIT, --Yes or no
+'HMO' BOOLEAN, --Yes or no
 PRIMARY KEY ('Appartment_Block'),
 FOREIGN KEY ('LeaseID') References 'Lease' ('LeaseID'),
 FOREIGN KEY ('ContractedJobsID') References 'ContractedJobs' ('ContractedJobsID'), 
 FOREIGN KEY ('BuildingID') References 'Building' ('BuildingID'),
-FOREIGN KEY ('OfficeID') References 'Office' ('OfficeID'),
+FOREIGN KEY ('OfficeID') References 'Office' ('OfficeID')
 --FOREIGN KEY ('ManagerID') References 'Manager' ('ManagerID')
 );
 
@@ -102,33 +97,29 @@ FOREIGN KEY ('OfficeID') References 'Office' ('OfficeID'),
 --Lease--
 CREATE TABLE IF NOT EXISTS 'Lease' (
 'LeaseID' int NOT NULL AUTO_INCREMENT UNIQUE,
-'Status'  BIT, --Yes or no (Paid or not paid)
+'Status'  BOOLEAN, --Yes or no (Paid or not paid)
 'Cost' int NOT NULL,
 FOREIGN KEY ('TenantID') References 'Tenant'('TenantID'),
-FOREIGN KEY ('ApartmentID') References 'Apartment' ('ApartmentID')
-
-
+FOREIGN KEY ('ApartmentID') References 'Apartment' ('ApartmentID'),
 FOREIGN KEY ('OfficeID') References 'Office' ('OfficeID'),
---BOTH???
 FOREIGN KEY ('ManagerID') References 'Manager' ('ManagerID'),
 PRIMARY KEY ('LeaseID')
 );
 
 
 
---Manager info derived from Person table maybe??---------------------------
 --Manager--
 CREATE TABLE IF NOT EXISTS 'Manager' (
 'ManagerID'int NOT NULL AUTO_INCREMENT UNIQUE,
-'MFname' char,
-'MSname' char,
+--
 PRIMARY KEY ('ManagerID'),
---FOREIGN KEY ('BuildingID') References 'Building' ('BuildingID'),
+FOREIGN KEY ('PersonID') References 'Person'('PersonID'),
 FOREIGN KEY ('ApartmentID') References 'Apartment' ('ApartmentID'),
 FOREIGN KEY ('ContractedJobsID') References 'ContractedJobs' ('ContractedJobsID'),
 FOREIGN KEY ('EmployeeID') References 'Employee' ('EmployeeID'),
 FOREIGN KEY ('LeaseID') References 'Lease' ('LeaseID')
 );
+
 
 
 
@@ -179,8 +170,7 @@ CREATE TABLE IF NOT EXISTS 'PaymentsOutgoing' (
 'PaymentsOutgoingREF' int NOT NULL AUTO_INCREMENT UNIQUE,
 PRIMARY KEY ('PaymentsOutgoingREF'),
 FOREIGN KEY ('BankAccountID') References 'BankAccount' ('BankAccountID'),
-FOREIGN KEY ('EmployeeID') References 'Employee' ('EmployeeID'),
---
+FOREIGN KEY ('EmployeeID') References 'Employee' ('EmployeeID')
 --
 );
 
@@ -195,3 +185,195 @@ FOREIGN KEY ('TenantID') References 'Tenant' ('TenantID'),
 --
 --
 );
+
+
+
+----------------------------------------------------------------------
+----------------------------QUOTES------------------------------------
+----------------------------------------------------------------------
+
+
+
+CREATE TABLE IF NOT EXISTS Person (
+PersonID int NOT NULL AUTO_INCREMENT UNIQUE,
+FName char,
+SName char,
+DoB date, 
+ContactNo varchar(11),
+NextOfKinNo varchar(11),
+Diabilities BOOLEAN,
+PRIMARY KEY (PersonID),
+FOREIGN KEY (TenantID) References Tenant (TenantID),
+FOREIGN KEY (EmployeeID) References Employee (EmployeeID),
+FOREIGN KEY (BankAccountID) References BankAccount (BankAccountID)
+);
+
+
+
+--Employee--
+CREATE TABLE IF NOT EXISTS Employee (
+EmployeeID int NOT NULL AUTO_INCREMENT UNIQUE,
+FOREIGN KEY (PersonID) References Person (PersonID),
+FOREIGN KEY (PaymentsOutgoingREF) References PaymentsOutgoing (PaymentsOutgoingREF),
+FOREIGN KEY (TechniciansID) References Technicians (TechniciansID),
+FOREIGN KEY (ManagerID) References Manager (ManagerID),
+FOREIGN KEY (EmployeeContractID) References EmployeeContract (EmployeeContractID),
+PRIMARY KEY (EmployeeID)
+);
+
+
+
+--Tenant--
+CREATE TABLE IF NOT EXISTS Tenant (
+TenantID int NOT NULL AUTO_INCREMENT UNIQUE,
+PRIMARY KEY (TenantID),
+FOREIGN KEY (PersonID) References Person (PersonID),
+FOREIGN KEY (VisitorID) References Visitor (VisitorID),
+FOREIGN KEY (LeaseID) References Lease (LeaseID),
+FOREIGN KEY (PaymentsIncomingREF) References PaymentsIncoming(PaymentsIncomingREF)
+);
+
+
+
+--Bank Account--
+CREATE TABLE IF NOT EXISTS Bank Account (
+BankAccountID int NOT NULL AUTO_INCREMENT UNIQUE,
+FOREIGN KEY (PersonID) References Person (PersonID),
+FOREIGN KEY (PaymentsOutgoingREF) References PaymentsOutgoing (PaymentsOutgoingREF),
+FOREIGN KEY (PaymentsIncomingREF) References PaymentsIncoming (PaymentsIncomingREF),
+AccountNumber smallint NOT NULL, 
+SortCode smallint NOT NULL
+);
+
+
+
+--Visitor
+CREATE TABLE IF NOT EXISTS Visitor (
+--FOREIGN KEY (TenantID) References Tenant (TenantID),
+FOREIGN KEY (LeaseID) References Lease (LeaseID),
+VFName char,
+VSName char,
+Duration TIME NOT NULL
+);
+
+
+
+--Building--
+CREATE TABLE IF NOT EXISTS Building (
+BuildingID int NOT NULL AUTO_INCREMENT UNIQUE,
+BuildingName varchar(3),
+Address char,
+PostCode varchar(7) UNIQUE,
+PRIMARY KEY (BuildingID),
+FOREIGN KEY (ApartmentID) References Apartment (ApartmentID)
+);
+
+
+
+--Apartment--
+CREATE TABLE IF NOT EXISTS Apartment (
+ApartmentID int NOT NULL AUTO_INCREMENT UNIQUE,
+Block varchar(32),
+Type varchar(32),
+HMO BOOLEAN, --Yes or no
+PRIMARY KEY (Appartment_Block),
+FOREIGN KEY (LeaseID) References Lease (LeaseID),
+FOREIGN KEY (ContractedJobsID) References ContractedJobs (ContractedJobsID), 
+FOREIGN KEY (BuildingID) References Building (BuildingID),
+FOREIGN KEY (OfficeID) References Office (OfficeID)
+--FOREIGN KEY (ManagerID) References Manager (ManagerID)
+);
+
+
+
+--Lease--
+CREATE TABLE IF NOT EXISTS Lease (
+LeaseID int NOT NULL AUTO_INCREMENT UNIQUE,
+Status  BOOLEAN, --Yes or no (Paid or not paid)
+Cost int NOT NULL,
+FOREIGN KEY (TenantID) References Tenant(TenantID),
+FOREIGN KEY (ApartmentID) References Apartment (ApartmentID),
+FOREIGN KEY (OfficeID) References Office (OfficeID),
+FOREIGN KEY (ManagerID) References Manager (ManagerID),
+PRIMARY KEY (LeaseID)
+);
+
+
+
+--Manager--
+CREATE TABLE IF NOT EXISTS Manager (
+ManagerIDint NOT NULL AUTO_INCREMENT UNIQUE,
+--
+PRIMARY KEY (ManagerID),
+FOREIGN KEY (PersonID) References Person(PersonID),
+FOREIGN KEY (ApartmentID) References Apartment (ApartmentID),
+FOREIGN KEY (ContractedJobsID) References ContractedJobs (ContractedJobsID),
+FOREIGN KEY (EmployeeID) References Employee (EmployeeID),
+FOREIGN KEY (LeaseID) References Lease (LeaseID)
+);
+
+
+
+--Office--
+CREATE TABLE IF NOT EXISTS Office (
+OfficeID int NOT NULL AUTO_INCREMENT UNIQUE,
+PRIMARY KEY (OfficeID),
+FOREIGN KEY (ApartmentID) References Apartment (ApartmentID),
+FOREIGN KEY (ManagerID) References Manager (ManagerID)
+);
+
+
+
+--Jobs/Contracts--
+CREATE TABLE IF NOT EXISTS ContractedJobs (
+ContractedJobsIDint NOT NULL AUTO_INCREMENT UNIQUE,
+PRIMARY KEY (ContractedJobsID),
+FOREIGN KEY (ManagerID) References Manager (ManagerID),
+FOREIGN KEY (TechniciansID) References Technicians (TechniciansID),
+FOREIGN KEY (ApartmentID) References Apartment (ApartmentID)
+);
+
+
+
+--Technicians--
+CREATE TABLE IF NOT EXISTS Technicians (
+TechniciansID int NOT NULL AUTO_INCREMENT UNIQUE,
+PRIMARY KEY (TechniciansID),
+FOREIGN KEY (EmployeeID) References Employee (EmployeeID),
+FOREIGN KEY (ContractedJobsID) References ContractedJobs (ContractedJobsID)
+);
+
+
+
+--EmployeeContract
+CREATE TABLE IF NOT EXISTS EmployeeContract (
+EmployeeContractID int NOT NULL AUTO_INCREMENT UNIQUE,
+EmpRate int NOT NULL,
+EmpHRsPWeek TIME,
+FOREIGN KEY (EmployeeID) References Employee (EmployeeID),
+PRIMARY KEY (EmployeeContractID)
+);
+
+
+
+--PaymentsOutgoing
+CREATE TABLE IF NOT EXISTS PaymentsOutgoing (
+PaymentsOutgoingREF int NOT NULL AUTO_INCREMENT UNIQUE,
+PRIMARY KEY (PaymentsOutgoingREF),
+FOREIGN KEY (BankAccountID) References BankAccount (BankAccountID),
+FOREIGN KEY (EmployeeID) References Employee (EmployeeID)
+--
+);
+
+
+
+--PaymentsIncoming
+CREATE TABLE IF NOT EXISTS PaymentsIncoming (
+PaymentsIncomingREF int NOT NULL AUTO_INCREMENT UNIQUE,
+PRIMARY KEY (PaymentsIncomingREF),
+FOREIGN KEY (BankAccountID) References BankAccount (BankAccountID),
+FOREIGN KEY (TenantID) References Tenant (TenantID),
+--
+--
+);
+
