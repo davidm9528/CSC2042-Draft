04e5a021529d798acd2d9b8ce06f242b838da9d1
David Mackenzie <davidm9528@gmail.com>
2019-11-04 23:33:23 +0000

    Add files via upload
    Comments added

--- Queries.sql
+++ Queries.sql
@@ -3,3 +3,9 @@ SELECT Person.FName, Person.SName, Person.ContactNo
 FROM Person 
 WHERE Person.Disabillities = TRUE AND (Tenant.TenantID > 0)
 AND (Employee.EmployeeID = 0); 
+
+--5
+SELECT Person.FName, Person.ContactNo, Person.SName, Office.OfficeID 
+FROM Person
+--Can't be completed with current table attribuites (no elms names in the table)
+
